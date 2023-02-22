d2aef53ecd44e1b5656725f34a3a0526323be620
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2023-02-22 17:10:03 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -15,6 +15,22 @@ const dealsSwiper = new Swiper('.deals-swiper', {
         nextEl: '.button-next',
         prevEl: '.button-prev',
     },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1.2,
+      },
+      // when window width is >= 480px
+      480: {
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 2.1,
+      },
+      1023: {
+        slidesPerView: 3.3,
+      }
+    }
 });
 
 let dealsSlides = document.querySelectorAll('.deals-slide');
@@ -55,6 +71,13 @@ radioLabel.forEach(el=>{
     })
 })
 
+
+
+let monBut = document.querySelector('.mon-but');
+monBut.onclick = function () {
+    monBut.classList.toggle('change')
+}
+
 let radioLabel2 = document.querySelectorAll('.q2');
 
 radioLabel2.forEach(el=>{
@@ -88,15 +111,38 @@ radioLabel4.forEach(el=>{
     }
 })
 
+let radioLabel4a = document.querySelectorAll('.sq2');
+
+radioLabel4a.forEach(el=>{
+    el.onclick = function(){
+        radioLabel4a.forEach(el=>{
+            el.classList.remove('active')
+        })
+        el.classList.add('active')
+    }
+})
+
+let mp3 = document.querySelectorAll('.sq4');
+
+mp3.forEach(el=>{
+    el.onclick = function(){
+        el.classList.toggle('active')
+    }
+})
+
 
 
 let sytAdd = document.querySelector('.syteAdd')
 let webAdd = document.querySelector('.webAdd')
 let markAdd = document.querySelector('.markAdd')
+let placeAdd = document.querySelector('.placeAdd')
 let howMth = document.querySelector('.howMth')
+let price = document.querySelector('.priseinner')
 
 let sytQuest = document.querySelector('.sytSec')
 let webQuest = document.querySelector('.webSec')
+let markettingQuest = document.querySelector('.marketting')
+let marketplace = document.querySelector('.marketplace')
 
 //скролл квиза
 
@@ -115,27 +161,51 @@ let somei = 5;
 
 sytAdd.onclick = function(){
     howMth.innerHTML = '06'
+    price.innerHTML = '15 000';
     sytQuest.classList.add('active');
     webQuest.classList.remove('active');
+    markettingQuest.classList.remove('active')
+    marketplace.classList.remove('active')
     somei = 5;
 }
 
 webAdd.onclick = function(){
-    howMth.innerHTML = '05'
+    price.innerHTML = '50 000';
+    howMth.innerHTML = '06'
     sytQuest.classList.remove('active');
+    markAdd.classList.remove('active')
+    marketplace.classList.remove('active')
     webQuest.classList.add('active');
-    somei = 4;
+    somei = 5;
 }
 
 markAdd.onclick = function(){
-    howMth.innerHTML = '02'
+    price.innerHTML = '30 000';
+    howMth.innerHTML = '03'
     sytQuest.classList.remove('active');
     webQuest.classList.remove('active');
-    somei = 1;
+    marketplace.classList.remove('active')
+    markettingQuest.classList.add('active')
+    somei = 2;
+}
+
+placeAdd.onclick = function(){
+    price.innerHTML = '30 000';
+    howMth.innerHTML = '08'
+    sytQuest.classList.remove('active');
+    webQuest.classList.remove('active');
+    markettingQuest.classList.remove('active')
+    marketplace.classList.add('active')
+    somei = 7;
 }
 
 
-quizButtonNext.onclick = function(){
+
+quizButtonNext.onclick = nextQuiz;
+
+quizButtonPrew.onclick = prevQuiz;
+
+function nextQuiz (){
     
     if(scrollItems[somei].classList.contains('active-quiz-slide')) {
         quizButtonNext.classList.add('not-active')
@@ -157,9 +227,10 @@ quizButtonNext.onclick = function(){
 
     }
     innerNum.innerHTML = indexActiveSlide +1;
+
 }
 
-quizButtonPrew.onclick = function(){
+function prevQuiz (){
     if(scrollItems[0].classList.contains('active-quiz-slide')) {
         quizButtonPrew.classList.add('not-active')
     } else {
@@ -194,3 +265,41 @@ textIn.forEach(el=>{
         block.value = el.value
     })
 })
+
+//открытие мобильного меню 
+
+let mobileMenuBtn = document.querySelector('.mobile-menu-button')
+let mobileMenu = document.querySelector('.mobile-menu')
+
+mobileMenuBtn.onclick = ()=>{
+    mobileMenu.classList.toggle('active')
+}
+
+//работа калькулятора
+
+
+
+
+
+function offsetPosition(element) {
+    var offsetLeft = 0, offsetTop = 0;
+    do {
+        offsetLeft += element.offsetLeft;
+        offsetTop  += element.offsetTop;
+    } while (element = element.offsetParent);
+    return offsetTop;
+}
+
+let scrollBtn = document.querySelectorAll('.scrollBtn');
+scrollBtn.forEach(el => {
+    let elem = el;
+    el.addEventListener('click', function(){
+        let data = elem.getAttribute('data-b');
+        let block = document.querySelector(data);
+        let offset = offsetPosition(block);
+        window.scrollTo({
+            top: offset - 150,
+            behavior: 'smooth'
+        });
+    })
+});
